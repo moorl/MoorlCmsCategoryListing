80128e9d56c06af45994987e17d1d03d8e3f658b
moorl <info@moorleiche.com>
2023-03-20 17:06:30 +0100

    patch 6.5

--- src/Core/Content/Category/Cms/CategoryListingCmsElementResolver.php
+++ src/Core/Content/Category/Cms/CategoryListingCmsElementResolver.php
@@ -4,10 +4,12 @@ namespace MoorlCmsCategoryListing\Core\Content\Category\Cms;
 
 use MoorlFoundation\Core\Content\Cms\FoundationListingCmsElementResolver;
 use Shopware\Core\Content\Category\CategoryCollection;
+use Shopware\Core\Content\Category\CategoryDefinition;
 use Shopware\Core\Content\Cms\Aggregate\CmsSlot\CmsSlotEntity;
 use Shopware\Core\Content\Cms\DataResolver\Element\ElementDataCollection;
 use Shopware\Core\Content\Cms\DataResolver\ResolverContext\ResolverContext;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
 
 class CategoryListingCmsElementResolver extends FoundationListingCmsElementResolver
 {
@@ -44,6 +46,15 @@ class CategoryListingCmsElementResolver extends FoundationListingCmsElementResol
             $categories = $listing->getEntities();
             if ($categories->count() > 1) {
                 $categories->sortByPosition();
+
+                $listing = new EntitySearchResult(
+                    CategoryDefinition::ENTITY_NAME,
+                    $listing->getTotal(),
+                    $categories,
+                    $listing->getAggregations(),
+                    $listing->getCriteria(),
+                    $context->getContext()
+                );
             }
         }
 
